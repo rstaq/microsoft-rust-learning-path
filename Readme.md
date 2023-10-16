cf69c6d5bc81dab72ae91cdcabe148341c317e6e
rw026 <robert@wenig.io>
2023-10-16 09:55:41 +0200

    update readme

---
+++ Readme.md
@@ -0,0 +1,25 @@
+# Microsoft Rust Learning Path Solutions
+
+Solutions for the exercises set in the
+[Microsoft Rust Learning Path](https://learn.microsoft.com/en-us/training/paths/rust-first-steps/)
+
+## Course Content
+
+* Install the tools you need to write your first lines of Rust code.
+* Learn basic concepts in Rust.
+* Learn how to handle errors.
+* Manage memory in Rust.
+* Use generic types and traits.
+* Set up modules for packages and crates.
+* Write and run automated tests.
+* Create a command-line program.
+
+## Exercises 
+
+- Exercise 01: [Write a function to build a car](car)
+- Exercise 02: [Work with compound types](car2)
+- Exercise 03: [Work with if/else conditions](car3)
+- Exercise 04: [Use a hash map to track orders](car4)
+- Exercise 05: [Use a loop to iterate on data](car5)
+- Exercise 06: [Use the Option type to deal with absence](person)
+- Exercise 07: [Use the Result type to handle errors](file)
--- car2/src/main.rs
+++ car2/src/main.rs
@@ -1,39 +1,73 @@
+#[derive(PartialEq, Debug)]
 // Declare Car struct to describe vehicle with four named fields
 struct Car {
     color: String,
-    transmission: Transmission,
-    convertible: bool,
-    mileage: u32,
+    motor: Transmission,
+    roof: bool,
+    age: (Age, u32)
 }
 
 #[derive(PartialEq, Debug)]
+enum Age { New, Used }
+
+#[derive(PartialEq, Debug)]
 // Declare enum for Car transmission type
-enum Transmission {
-    Manual,
-    SemiAuto,
-    Automatic,
+enum Transmission { Manual, SemiAuto, Automatic }
+
+// Get the car quality by testing the value of the input argument
+// - miles (u32)
+// Create a tuple for the car quality with the Age ("New" or "Used") and mileage
+// Return a tuple with the arrow `->` syntax
+fn car_quality (miles: u32) -> (Age, u32) {
+
+    // Declare and initialize the return tuple value
+    // For a new car, set the miles to 0
+    let quality = (Age::New, miles);
+
+    // Return the completed tuple to the caller
+    quality
 }
 
-// Build a "Car" by using values from the input arguments
-// - Color of car (String)
-// - Transmission type (enum value)
-// - Convertible (boolean, true if car is a convertible)
-fn car_factory(color: String, transmission: Transmission, convertible: bool) -> Car {
-
-    // Use the values of the input arguments
-    // All new cars always have zero mileage
-    Car{color, transmission, convertible, mileage: 0}
+// Build a new "Car" using the values of four input arguments
+// - color (String)
+// - motor (Transmission enum)
+// - roof (boolean, true if the car has a hard top roof)
+// - miles (u32)
+// Call the car_quality(miles) function to get the car age
+// Return an instance of a "Car" struct with the arrow `->` syntax
+fn car_factory(color: String, motor: Transmission, roof: bool, miles: u32) -> Car {
+    // Create a new "Car" instance as requested
+    // - Bind first three fields to values of input arguments
+    // - "age" field calls "car_quality" function with "miles" input argument
+    Car {
+        color: color,
+        motor: motor,
+        roof: roof,
+        age: car_quality(miles)
+    }
 }
 
 fn main() {
-    // We have orders for three new cars!
-    // We'll declare a mutable car variable and reuse it for all the cars
-    let mut car = car_factory(String::from("Red"), Transmission::Manual, false);
-    println!("Car 1 = {}, {:?} transmission, convertible: {}, mileage: {}", car.color, car.transmission, car.convertible, car.mileage);
+    // Create car color array
+    let colors = ["Blue", "Green", "Red", "Silver"];
 
-    car = car_factory(String::from("Silver"), Transmission::Automatic, true);
-    println!("Car 2 = {}, {:?} transmission, convertible: {}, mileage: {}", car.color, car.transmission, car.convertible, car.mileage);
+    // Declare the car type and initial values
+    let mut car: Car;
+    let mut engine = Transmission::Manual;
 
-    car = car_factory(String::from("Yellow"), Transmission::SemiAuto, false);
-    println!("Car 3 = {}, {:?} transmission, convertible: {}, mileage: {}", car.color, car.transmission, car.convertible, car.mileage);
+    // Order 3 cars, one car for each type of transmission
+
+    // Car order #1: New, Manual, Hard top
+    car = car_factory(colors[0].to_string(), engine, true, 0);
+    println!("Car order 1: {:?}, Hard top = {}, {:?}, {}, {} miles", car.age.0, car.roof, car.motor, car.color, car.age.1);
+
+    // Car order #2: Used, Semi-automatic, Convertible
+    engine = Transmission::SemiAuto;
+    car = car_factory(colors[1].to_string(), engine, false, 100);
+    println!("Car order 2: {:?}, Hard top = {}, {:?}, {}, {} miles", car.age.0, car.roof, car.motor, car.color, car.age.1);
+
+    // Car order #3: Used, Automatic, Hard top
+    engine = Transmission::Automatic;
+    car = car_factory(colors[2].to_string(), engine, true, 200);
+    println!("Car order 3: {:?}, Hard top = {}, {:?}, {}, {} miles", car.age.0, car.roof, car.motor, car.color, car.age.1);
 }
